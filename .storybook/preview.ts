281cff149d038b987cb4635c5cab821c0f64bf3f
wooyoung6685 <wooyoung6685@gmail.com>
2023-05-13 22:29:21 +0900

    [config]font,global style,theme,storybook 셋팅

---
+++ .storybook/preview.ts
@@ -0,0 +1,30 @@
+import type { Preview } from '@storybook/react';
+import { withThemeFromJSXProvider } from '@storybook/addon-styling';
+import { ThemeProvider } from 'styled-components';
+import { myTheme } from '../src/theme/theme';
+import GlobalStyles from '../src/styles/global';
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    GlobalStyles,
+    themes: {
+      light: myTheme,
+    },
+    defaultTheme: 'light',
+    Provider: ThemeProvider,
+  }),
+];
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from './styles/global';
+import { myTheme } from './theme/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={myTheme}>
+      <App />
+      <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>,
 );
 
