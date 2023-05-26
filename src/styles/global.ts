4e7afb377ad9c3bfde482dfd5332a1130c25e0c5
wooyoung6685 <wooyoung6685@gmail.com>
2023-05-26 17:52:11 +0900

    [config] fontSize rem단위로 변경

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,6 +6,11 @@ export const GlobalStyles = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+  
+  html {
+    font-size: 62.5%; // 1rem = 10px 로 변경
+  }
+
 
   body {
     font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;  -webkit-font-smoothing: antialiased;
