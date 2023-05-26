6f3efef16898778c5b356c81885057282de5319f
wooyoung6685 <wooyoung6685@gmail.com>
2023-05-26 17:50:42 +0900

    [config] LoginForm 컴포넌트 생성

---
+++ src/components/form/organisms/LoginForm/LoginForm.tsx
@@ -0,0 +1,94 @@
+import { Button } from 'components/form/atoms/Button';
+import { Inputs } from 'components/form/molecules/Inputs';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+import { ButtonWrapper, Form, LoginFormWrapper } from './LoginForm.styled';
+import type { VFC } from 'common/utils/types';
+import type { SubmitHandler } from 'react-hook-form';
+
+interface LoginFormProps {
+  email: string;
+  password: string;
+}
+
+export const LoginForm: VFC = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    resetField,
+    formState: { errors, isValid },
+  } = useForm<LoginFormProps>({ mode: 'onChange' });
+
+  const onSubmit: SubmitHandler<LoginFormProps> = (data) => {
+    console.log(data);
+  };
+
+  const emailValue: string = watch('email', '');
+  const passwordValue: string = watch('password', '');
+
+  const handleEmailResetClick = () => {
+    resetField('email'); // 인풋값 초기화
+  };
+
+  const isErrorsEmpty = Object.keys(errors).length === 0;
+
+  const inputValue = watch('email') && watch('password');
+
+  const isFormValid = inputValue && isErrorsEmpty;
+
+  console.log(isValid);
+  console.log(errors);
+
+  return (
+    <LoginFormWrapper>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Inputs
+          onClick={handleEmailResetClick}
+          labelRequired
+          typingrequired
+          placeholder='이메일 형식을 입력해주세요.'
+          {...register('email', {
+            required: '이메일은 필수로 입력해주세요.',
+            pattern: {
+              value: /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/,
+              message: '이메일 형식을 입력해주세요.',
+            },
+          })}
+          valueLength={emailValue.length}
+          maximum={50}
+        >
+          이메일
+        </Inputs>
+        <Inputs
+          onClick={handleEmailResetClick}
+          labelRequired
+          typingrequired
+          placeholder='사용하실 비밀번호를 입력해주세요.'
+          type='password'
+          {...register('password', {
+            required: '비밀번호는 필수로 입력해주세요.',
+            pattern: {
+              value: /^(?=.*[A-Za-z])(?=.*[\d\W]).{4,12}$/,
+              message: '비밀번호는 필수로 입력해주세요',
+            },
+          })}
+          valueLength={passwordValue.length}
+          maximum={50}
+        >
+          비밀번호
+        </Inputs>
+        <ButtonWrapper>
+          {isFormValid ? (
+            <Button backgroundColor='#FF8D4E' color='#FFFFFF'>
+              로그인
+            </Button>
+          ) : (
+            <Button>로그인</Button>
+          )}
+        </ButtonWrapper>
+      </Form>
+    </LoginFormWrapper>
+  );
+};
