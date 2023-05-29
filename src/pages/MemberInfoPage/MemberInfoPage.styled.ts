f0a7da21bf47f5c96457c56d06097953e53aabcd
wooyoung6685 <wooyoung6685@gmail.com>
2023-05-29 20:39:31 +0900

    [config] MemberInfoPage 컴포넌트 생성

---
+++ src/pages/MemberInfoPage/MemberInfoPage.styled.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const MemberInfoPageWrapper = styled.div`
+  max-width: 67.5rem;
+  width: 100%;
+`;
+
+export const MemberInfoPageTitle = styled.div`
+  width: 100%;
+  height: 10.8rem;
+  font-weight: 500;
+  font-size: 2rem;
+  color: ${(porps): string => porps.theme.palette.black.main};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ProgressBarInner = styled.div`
+  width: 100%;
+  height: 0.4rem;
+  background: ${(porps): string => porps.theme.palette.primary.main};
+`;
---
+++ src/pages/MemberInfoPage/MemberInfoPage.tsx
@@ -0,0 +1,21 @@
+import { MemberInfoForm } from 'components/form/organisms/MemberInfoForm';
+import { ProgressBar } from 'pages/SignUpPage/SignUpPage.styled';
+import React from 'react';
+import {
+  MemberInfoPageTitle,
+  MemberInfoPageWrapper,
+  ProgressBarInner,
+} from './MemberInfoPage.styled';
+import type { VFC } from 'common/utils/types';
+
+export const MemberInfoPage: VFC = () => {
+  return (
+    <MemberInfoPageWrapper>
+      <MemberInfoPageTitle>회원 정보 입력</MemberInfoPageTitle>
+      <ProgressBar>
+        <ProgressBarInner />
+      </ProgressBar>
+      <MemberInfoForm />
+    </MemberInfoPageWrapper>
+  );
+};
