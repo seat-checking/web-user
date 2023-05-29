6f310a83301af930036ec58a16f258be87fc7606
wooyoung6685 <wooyoung6685@gmail.com>
2023-05-29 23:33:21 +0900

    [config] 확장자 ts로 변경

--- src/components/layout/tab/Tabs/Tabs.styled.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Wrapper = styled.div`
-  max-width: 675px;
-  display: flex;
-  flex-direction: row;
-  border-bottom: 1px solid ${({ theme }) => theme.palette.grey[50]};
-`;
