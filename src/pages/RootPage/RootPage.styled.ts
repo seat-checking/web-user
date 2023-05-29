b6b8e4c54c41013bc168588c3e5f97f18919445a
wooyoung6685 <wooyoung6685@gmail.com>
2023-05-29 20:39:58 +0900

    [config] RootPage 컴포넌트 생성

---
+++ src/pages/RootPage/RootPage.styled.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const RootPageWrapper = styled.div`
+  max-width: 67.5rem;
+  width: 100%;
+`;
+
+export const LogoWrapper = styled.div`
+width: 80%
+display: flex;
+justify-content: center;
+margin-top: 6.8rem;
+`;
+
+export const ButtonWrapper = styled.div`
+  text-align: center;
+  width: 100%;
+  margin-top: 14.1rem;
+`;
