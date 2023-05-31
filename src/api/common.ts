e85e6da012fca7482224ca22a8d3643835cb5240
wooyoung6685 <wooyoung6685@gmail.com>
2023-05-31 23:37:45 +0900

    [config] user관련 api 폴더 생성 및 로직 작성

---
+++ src/api/common.ts
@@ -0,0 +1,25 @@
+export const HOST = 'https://2989-211-38-162-27.ngrok-free.app/api';
+// 프로젝트에 .env 파일을 만들어서 이 파일 안에 보안상 중요한 정보 & 상황에 따라 바뀔 수 있는 값을 따로 빼내서 관리하는 방법
+// dotenv -> 위 방법을 도와주는 라이브러리
+
+/**
+ * 200 응답 구조
+ *
+ * result 값이 존재하지 않음
+ */
+export interface SuccessOkWithoutResultResponse {
+  isSuccess: boolean;
+  code: string;
+  message: string;
+  timestamp: string;
+  status: number;
+}
+
+/**
+ * 200 응답 구조
+ *
+ * result 값이 존재
+ */
+export interface SuccessOkResponse<T> extends SuccessOkWithoutResultResponse {
+  result: T;
+}
