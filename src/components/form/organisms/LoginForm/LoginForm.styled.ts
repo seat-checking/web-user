79bdff5c73ac298c4951b9e3468edeba4ae7d868
wooyoung6685 <wooyoung6685@gmail.com>
2023-05-31 23:50:44 +0900

    [config] 로그인 에러 메세지 구역 설정

--- src/components/form/organisms/LoginForm/LoginForm.styled.ts
+++ src/components/form/organisms/LoginForm/LoginForm.styled.ts
@@ -1,3 +1,4 @@
+import errorIcon from 'assets/icons/error.svg';
 import styled from 'styled-components';
 
 export const LoginFormWrapper = styled.div`
@@ -12,4 +13,29 @@ export const Form = styled.form`
 export const ButtonWrapper = styled.div`
   text-align: center;
   padding-top: 5rem;
+`;
+
+export const ErrorMessageWrapper = styled.div`
+  width: 100%;
+  height: 1.7rem;
+  display: flex;
+  align-items: center; 
+  justify-content: center; /
+`;
+
+export const ErrorIcon = styled.div`
+  width: 1.6rem;
+  height: 1.6rem;
+  background-image: url(${errorIcon});
+  background-repeat: no-repeat;
+  background-size: cover;
+  cursor: pointer;
+  display: inline-block;
+  margin-right: 0.4rem;
+`;
+
+export const ErrorMessage = styled.span`
+  font-weight: 400;
+  font-size: 14px;
+  color: ${(porps): string => porps.theme.palette.grey[500]};
 `;
