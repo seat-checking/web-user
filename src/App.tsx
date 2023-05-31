839a5da63dbc90f97aeb91e9fea76c5b3f416776
wooyoung6685 <wooyoung6685@gmail.com>
2023-05-31 23:36:45 +0900

    [config] router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { SignUpForm } from 'components/form/organisms/SignUpForm';
-import { Example } from 'pages/Example';
+import { LoginForm } from 'components/form/organisms/LoginForm';
+import { LoginPage } from 'pages/LoginPage';
+import { MemberInfoPage } from 'pages/MemberInfoPage';
+import { RootPage } from 'pages/RootPage';
+import { SignUpPage } from 'pages/SignUpPage';
+import { Route, Routes } from 'react-router-dom';
 import type { VFC } from 'common/utils/types';
 
 const App: VFC = () => {
   return (
     <div>
-      <SignUpForm />
+      <Routes>
+        <Route path='/signup/first' element={<SignUpPage />} />
+        <Route path='/signup/second' element={<MemberInfoPage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from 'styles/global';
 import App from './App';
@@ -13,10 +14,12 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={myTheme}>
-      <App />
-      <GlobalStyles />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={myTheme}>
+        <App />
+        <GlobalStyles />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
