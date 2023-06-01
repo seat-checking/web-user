eae25111729fa4cd50d9ed43542b9e106355bb4d
wooyoung6685 <wooyoung6685@gmail.com>
2023-06-01 16:45:28 +0900

    [config] 음식점 리스트 컴포넌트 생성

---
+++ src/components/item/FoodListItem/FoodListItem.styled.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const StoreListDiv = styled.div`
+  max-width: 67.5rem;
+  height: 9.6rem;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+`;
+
+export const StoreListImgDiv = styled.div`
+  padding: 1.2rem;
+`;
+export const StoreListImg = styled.img`
+  width: 7.2rem;
+  height: 7.2rem;
+  border-radius: 0.6rem;
+`;
+export const StoreListDescription = styled.div`
+  padding-left: 0.6rem;
+  line-height: 1.8rem;
+`;
+export const StoreListStoreName = styled.p`
+  font-weight: 700;
+  font-size: 1.6rem;
+  margin-bottom: 0.5rem;
+`;
+export const StoreListStoreIntroduction = styled.span`
+  font-weight: 0.5rem;
+  font-size: 1.4rem;
+`;
