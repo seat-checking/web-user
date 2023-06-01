ab8e266ebf207b87d3fe454a21c7c00d595842ff
wooyoung6685 <wooyoung6685@gmail.com>
2023-06-01 16:42:57 +0900

    [config] ListTab,Navigation 임포트

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { ListTab } from 'components/ListTab';
-import { SignUpForm } from 'components/form/organisms/SignUpForm';
-import { Example } from 'pages/Example';
+import { Navigation } from 'components/Navigation';
 import type { VFC } from 'common/utils/types';
 
 const App: VFC = () => {
   return (
     <div>
       <ListTab />
+      <Navigation />
     </div>
   );
 };
