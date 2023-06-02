df1f037b48b48ef61cd91be43a278d3801ba7e6f
wooyoung6685 <wooyoung6685@gmail.com>
2023-06-02 16:35:45 +0900

    [config] 스토어 아이템 컴포넌트 생성

---
+++ src/components/store/StoreItem/StoreItem.tsx
@@ -0,0 +1,34 @@
+import {
+  StoreItemDescription,
+  StoreItemDiv,
+  StoreItemImg,
+  StoreItemImgDiv,
+  StoreItemIntroduction,
+  StoreItemName,
+} from 'components/store/StoreItem/StoreItem.styled';
+import React from 'react';
+import type { VFC } from 'common/utils/types';
+
+interface StoreItemProps {
+  src: string;
+  storeName: string;
+  introduction: string;
+}
+
+export const StoreItem: VFC<StoreItemProps> = ({
+  src,
+  storeName,
+  introduction,
+}) => {
+  return (
+    <StoreItemDiv>
+      <StoreItemImgDiv>
+        <StoreItemImg src={src} />
+      </StoreItemImgDiv>
+      <StoreItemDescription>
+        <StoreItemName>{storeName}</StoreItemName>
+        <StoreItemIntroduction>{introduction}</StoreItemIntroduction>
+      </StoreItemDescription>
+    </StoreItemDiv>
+  );
+};
