2c225c2ea82600e0b9b759ec2891fcaa73228a1d
wooyoung6685 <wooyoung6685@gmail.com>
2023-06-08 16:47:30 +0900

    [add] 스토어 리스트 api연동

--- src/components/store/storeList/FoodList/FoodList.tsx
+++ src/components/store/storeList/FoodList/FoodList.tsx
@@ -1,16 +1,12 @@
+import { getStoreList } from 'api/store/storeApi';
 import { StoreItem } from 'components/store/StoreItem';
+import { useEffect, useState } from 'react';
+import type { StoreUser } from 'api/store/storeApi';
+
 import type { VFC } from 'common/utils/types';
 
-interface ItemsProps {
-  id: number;
-  image: string;
-  storeName: string;
-  introduction: string;
-}
-
 export const FoodList: VFC = () => {
-  /*
-  const [stores, setStores] = useState<ItemsProps[]>([]);
+  const [stores, setStores] = useState<StoreUser[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<null | string>(null);
 
@@ -18,57 +14,32 @@ export const FoodList: VFC = () => {
     const getStoreData = async () => {
       try {
         setLoading(true);
-        const response = await axios.get('/api/');
-        setStores(response.data);
+        const resData = await getStoreList({ category: 'RESTAURANT' });
+        setStores(resData.result.storeList);
+        setLoading(false);
       } catch (e) {
-        // 문제 발생시 처리 로직
         setError('서버에서 데이터를 받아오지 못했습니다');
+        setLoading(false);
       }
-      setLoading(false);
     };
     getStoreData();
   }, []);
 
   if (loading) {
-    return; // 로딩시 로직
+    return <div>Loading...</div>;
   }
+
   if (error) {
-    return; // 에러 발생 로직
+    return <div>Error: {error}</div>;
   }
-
-  if (stores === null) {
-    return null;
-  }
-  */
-
-  const items: ItemsProps[] = [
-    {
-      id: 1,
-      image: 'assets/images/storeimage.png',
-      storeName: '가게이름2',
-      introduction: '한줄소개',
-    },
-    {
-      id: 2,
-      image: 'assets/images/storeimage.svg',
-      storeName: '가게이름2',
-      introduction: '한줄소개',
-    },
-    {
-      id: 3,
-      image: 'assets/images/storeimage.svg',
-      storeName: '가게이름2',
-      introduction: '한줄소개',
-    },
-  ];
 
   return (
     <>
-      {items.map((store) => (
+      {stores.map((store) => (
         <StoreItem
-          key={store.id}
-          src={store.image}
-          storeName={store.storeName}
+          key={store.name}
+          src={store.mainImage}
+          storeName={store.name}
           introduction={store.introduction}
         />
       ))}
