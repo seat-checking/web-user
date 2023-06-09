e13043f8991415bf6110805f2fe15da3376fff09
wooyoung6685 <wooyoung6685@gmail.com>
2023-06-09 17:48:05 +0900

    [remove] 데드코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import type { VFC } from 'common/utils/types';
 
 const App: VFC = () => {
   return (
-    <div style={{ fontSize: 23 }}>
-      {/* 실시간 좌석 확인 */}
-      {/* <Example /> */}
+    <div>
       <StoreDetail />
     </div>
   );
