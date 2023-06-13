2df8d5932b778b5a9f1ab75e1cfeb20b323f78ec
wooyoung6685 <wooyoung6685@gmail.com>
2023-06-13 17:18:05 +0900

    [add] 검색바 api연동

--- src/components/store/SearchBar/SearchBar.tsx
+++ src/components/store/SearchBar/SearchBar.tsx
@@ -1,5 +1,6 @@
 import { getSeachList } from 'api/store/storeApi';
 import { InputResetIcon } from 'components/form/atoms/InputResetIcon';
+import { Spinner } from 'components/layout/Spinner';
 import {
   ResetbtnWrapper,
   SearchBarContainer,
@@ -9,55 +10,59 @@ import {
 } from 'components/store/SearchBar/SearchBar.styled';
 import { StoreItem } from 'components/store/StoreItem';
 import { BackButtonIcon } from 'pages/LoginPage/LoginPage.styled';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import type { StoreUser } from 'api/store/storeApi';
-
 import type { VFC } from 'common/utils/types';
-import type { ChangeEvent } from 'react';
+
+import type { ChangeEvent, KeyboardEvent } from 'react';
 
 export const SearchBar: VFC = () => {
   const [value, setValue] = useState<string>('');
-  const [stores, setStores] = useState<StoreUser[]>([]);
+  const [Stores, setStores] = useState<StoreUser[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<null | string>(null);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
+
   const handleValueResetClick = () => {
     setValue('');
   };
 
   const showResetIcon = value.length > 0; // 인풋값이 있는 경우에만 리셋 아이콘을 보여줌
 
-  useEffect(() => {
-    const getSearchData = async () => {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate(-1);
+  };
+
+  const handleKeyDown = async (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
       try {
         setLoading(true);
         const resData = await getSeachList({ name: value });
         setStores(resData.result.storeList);
         setLoading(false);
-      } catch (e) {
+      } catch (err) {
         setError('서버에서 데이터를 받아오지 못했습니다');
         setLoading(false);
       }
-    };
-    getSearchData();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+    }
+  };
 
   return (
     <SearchBarContainer>
       <SearchBarWrapper>
-        <BackButtonIcon />
+        <BackButtonIcon onClick={handleButtonClick} />
         <SearchInputWrapper>
           <SearchInput
             placeholder='검색어를 입력하세요'
             onChange={handleChange}
             value={value}
+            onKeyDown={handleKeyDown} // 엔터 키 이벤트 처리
           />
           {showResetIcon && (
             <ResetbtnWrapper>
@@ -67,15 +72,18 @@ export const SearchBar: VFC = () => {
         </SearchInputWrapper>
       </SearchBarWrapper>
       {error && <div>Error: {error}</div>}
-      {!error &&
-        stores.map((store) => (
+      {loading ? (
+        <Spinner />
+      ) : (
+        Stores.map((store) => (
           <StoreItem
-            key={store.name}
+            key={store.id}
             src={store.mainImage}
             storeName={store.name}
             introduction={store.introduction}
           />
-        ))}
+        ))
+      )}
     </SearchBarContainer>
   );
 };
