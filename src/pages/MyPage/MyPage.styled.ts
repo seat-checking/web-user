1ae7fae9443dc57091a5445acec243fba07c51f8
wooyoung6685 <wooyoung6685@gmail.com>
2023-06-14 10:56:10 +0900

    [design] css수정

--- src/pages/MyPage/MyPage.styled.ts
+++ src/pages/MyPage/MyPage.styled.ts
@@ -2,7 +2,6 @@ import backIcon from 'assets/icons/backBtn.svg';
 import styled from 'styled-components';
 
 export const MyPageWrapper = styled.div`
-  max-width: 67.5rem;
   width: 100%;
 `;
 
@@ -10,7 +9,7 @@ export const MypageHeader = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 80%;
+  width: 90%;
   margin: 0 auto;
 `;
 export const MyPageTitle = styled.div`
@@ -22,6 +21,7 @@ export const MyPageTitle = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-right: 3rem;
 `;
 
 export const BackButton = styled.div`
@@ -33,11 +33,11 @@ export const BackButton = styled.div`
 
 export const UserInfoWrapper = styled.div`
   display: flex;
-  width: 80%;
+  width: 90%;
   height: 9rem;
   margin: 0 auto;
   align-items: center;
-  border-bottom: 1px solid #eff0f5;
+  border-bottom: 0.1rem solid #eff0f5;
 `;
 
 export const UserInfoImg = styled.img`
@@ -54,7 +54,7 @@ export const UserinfoNickname = styled.div`
 `;
 
 export const UserSeatInfoWrapper = styled.div`
-  width: 80%;
+  width: 90%;
   background-color: ${(props): string => props.theme.palette.primary.orange};
   border-radius: 0.8rem;
   margin: 0 auto;
@@ -107,32 +107,37 @@ export const SeatPlaceInfo = styled.div`
   margin-bottom: 1.6rem;
 `;
 export const MyPageMenuFristWrapper = styled.div`
-  width: 80%;
-  border-top: 1px solid #eff0f5;
-  border-bottom: 1px solid #eff0f5;
+  width: 90%;
+  border-top: 0.1rem solid #eff0f5;
   margin: 0 auto;
   margin-top: 1.6rem;
 `;
 export const MyPageMenuSecondWrapper = styled.div`
-  width: 80%;
+  width: 90%;
+  border-top: 0.1rem solid #eff0f5;
   margin: 0 auto;
   margin-top: 1.6rem;
 `;
 
 export const MyPageMenuListWrapper = styled.div`
   display: flex;
-  margin-top: 2.8rem;
-  margin-bottom: 2.8rem;
+  align-items: center;
   cursor: pointer;
+  height: 5.6rem;
+
+  &:first-child {
+    margin-top: 1.2rem;
+  }
 `;
 
 export const MyPageMenuList = styled.div`
   font-weight: 400;
   font-size: 1.6rem;
   color: ${(props): string => props.theme.palette.black.main};
-  margin-left: 1.6rem;
 `;
 export const UserInfoIcon = styled.img`
   width: 1.6rem;
   height: 1.6rem;
+  margin-left: 2rem;
+  margin-right: 2rem;
 `;
