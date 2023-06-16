ec7167b32d289e4841bb345692ba30612174aa1a
wooyoung6685 <wooyoung6685@gmail.com>
2023-06-16 15:54:31 +0900

    [add] 라우팅 설정

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,6 +1,6 @@
 import { PATH } from 'common/utils/constants';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import {
   NavigationIconWrapper,
   NavigationMyicon,
@@ -12,33 +12,21 @@ import {
 import type { VFC } from 'common/utils/types';
 
 export const Navigation: VFC = () => {
-  const [activeIcon, setActiveIcon] = useState('store');
-
-  const handleClick = (iconName: string) => {
-    setActiveIcon(iconName);
-  };
   return (
     <NavigationWrapper>
       <NavigationIconWrapper>
-        <Link to={`/${PATH.storeList}`}>
-          <NavigationStoreIcon
-            onClick={() => handleClick('store')}
-            active={activeIcon === 'store'}
-          />
-        </Link>
-        <Link to='/search'>
+        <NavLink to={`/${PATH.storeList}`}>
+          {({ isActive }) => <NavigationStoreIcon active={isActive} />}
+        </NavLink>
+        <NavLink to={`/${PATH.search}`}>
           <NavigationSearchIcon />
-        </Link>
-        <NavigationNotificationIcon
-          onClick={() => handleClick('notification')}
-          active={activeIcon === 'notification'}
-        />
-        <Link to='/myPage'>
-          <NavigationMyicon
-            onClick={() => handleClick('my')}
-            active={activeIcon === 'my'}
-          />
-        </Link>
+        </NavLink>
+        <NavLink to={`/${PATH.notification}`}>
+          {({ isActive }) => <NavigationNotificationIcon active={isActive} />}
+        </NavLink>
+        <NavLink to={`/${PATH.myPage}`}>
+          {({ isActive }) => <NavigationMyicon active={isActive} />}
+        </NavLink>
       </NavigationIconWrapper>
     </NavigationWrapper>
   );
