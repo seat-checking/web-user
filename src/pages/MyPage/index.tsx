a8f33444f6ae8b693d9cd1f022d324cde1b67301
wooyoung6685 <wooyoung6685@gmail.com>
2023-06-16 15:53:00 +0900

    [add] mypage 컴포넌트생성

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,8 +1,9 @@
+import { Navigation } from 'components/Navigation';
 import type { VFC } from 'common/utils/types';
 
 /**
  * 마이 페이지 컴포넌트 입니다.
  */
 export const MyPage: VFC = () => {
-  return <div>마이 페이지</div>;
+  return <Navigation />;
 };
