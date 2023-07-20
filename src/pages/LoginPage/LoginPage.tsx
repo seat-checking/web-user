161b554dce40c1e7dd98ae417dd0deb0968aaa7f
wooyoung6685 <wooyoung6685@gmail.com>
2023-07-20 18:19:35 +0900

    [remove] 데드코드 삭제

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,4 @@
 import { LoginForm } from 'components/form/organisms/LoginForm';
-
-import { Modal } from 'components/layout/Modal';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
   BackButtonIcon,
@@ -13,17 +10,9 @@ import type { VFC } from 'common/utils/types';
 
 export const LoginPage: VFC = () => {
   const navigate = useNavigate();
-  const [modalOpen, setModalOpen] = useState(false);
 
   const handleback = (): void => {
     navigate(-1);
-  };
-  const openModal = () => {
-    setModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalOpen(false);
   };
 
   return (
@@ -33,12 +22,6 @@ export const LoginPage: VFC = () => {
       </LoginPageHeader>
       <LogoBox>로고 썸네일 등 </LogoBox>
       <LoginForm />
-      <button type='button' onClick={openModal} style={{ fontSize: '40px' }}>
-        모달
-      </button>
-      <Modal isOpen={modalOpen} onClose={closeModal}>
-        모달창
-      </Modal>
     </LoginPageWrapper>
   );
 };
