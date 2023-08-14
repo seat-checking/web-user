e7735ec1935722353bf4bfc70f7b79a3cd416dd4
wooyoung6685 <wooyoung6685@gmail.com>
2023-08-14 15:32:01 +0900

    [design] css수정

--- src/pages/SeatUsePage/SeatUsePage.styled.ts
+++ src/pages/SeatUsePage/SeatUsePage.styled.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const HeaderWrapper = styled.div`
   width: 100%;
+  border-bottom: 0.1rem solid ${(props): string => props.theme.palette.grey[50]};
 `;
 
 export const ReservationPageHeader = styled.div`
