9f469923d3679498ee2d37592b95ba4c71032967
wooyoung6685 <wooyoung6685@gmail.com>
2023-08-14 16:25:22 +0900

    [add] getFormattedMonthAndDay 함수 수정

--- src/components/reservationStatus/reservationList/ApprovedList/ApprovedList.tsx
+++ src/components/reservationStatus/reservationList/ApprovedList/ApprovedList.tsx
@@ -19,8 +19,8 @@ export const getFormattedTime = (isoString: string) => {
 };
 export const getFormattedMonthAndDay = (isoString: string) => {
   const date = new Date(isoString);
-  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-  const day = String(date.getUTCDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
   return `${month}월 ${day}일`;
 };
 export const APPROVED_LIST_QUERY_KEY = ['approvedList'];
