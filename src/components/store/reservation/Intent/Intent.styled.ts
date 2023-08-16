ffc51fc2b75c9d12ef28985bc05787ceb38a7b29
wooyoung6685 <wooyoung6685@gmail.com>
2023-08-16 14:39:23 +0900

    [add] Intent 컴포넌트 생성

--- src/components/store/reservation/Intent/Intent.styled.ts
+++ src/components/store/reservation/Intent/Intent.styled.ts
@@ -3,3 +3,7 @@ import styled from 'styled-components';
 export const IntentWrapper = styled.div`
   width: 100%;
 `;
+
+export const Form = styled.form`
+  width: 100%;
+`;
--- src/components/store/reservation/Intent/Intent.tsx
+++ src/components/store/reservation/Intent/Intent.tsx
@@ -1,5 +1,20 @@
+import { Inputs } from 'components/form/molecules/Inputs';
+import {
+  Form,
+  IntentWrapper,
+} from 'components/store/reservation/Intent/Intent.styled';
 import React from 'react';
 
 export const Intent = () => {
-  return <>ds</>;
+  return (
+    <IntentWrapper>
+      <Inputs
+        labelRequired
+        placeholder='사용 목적을 입력해주세요'
+        valueLength={0}
+      >
+        사용목적
+      </Inputs>
+    </IntentWrapper>
+  );
 };
