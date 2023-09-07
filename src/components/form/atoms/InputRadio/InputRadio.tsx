41c6506410757594ad0a2f0a41f2e2a99dd85763
wooyoung6685 <wooyoung6685@gmail.com>
2023-09-07 12:57:38 +0900

    [add]

--- src/components/form/atoms/InputRadio/InputRadio.tsx
+++ src/components/form/atoms/InputRadio/InputRadio.tsx
@@ -11,6 +11,7 @@ interface RadioProps extends React.HtmlHTMLAttributes<HTMLInputElement> {
   name: string; // radio group 내에서 동일한 이름 사용해야함
   value: string;
   label: string;
+  size?: 'small' | 'medium';
   checked?: boolean;
 }
 
@@ -20,21 +21,22 @@ interface RadioProps extends React.HtmlHTMLAttributes<HTMLInputElement> {
 // eslint-disable-next-line react/display-name
 export const InputRadio = forwardRef(
   (
-    { id, name, value, label, checked, ...rest }: RadioProps,
+    { id, name, value, label, checked, size = 'medium', ...rest }: RadioProps,
     ref: Ref<HTMLInputElement>,
   ) => {
     return (
       <Label htmlFor={id}>
         <RadioInput
-          checked={checked}
           type='radio'
           id={id}
           name={name}
           value={value}
           ref={ref}
+          checked={checked}
+          $size={size}
           {...rest}
         />
-        <LabelText>{label}</LabelText>
+        <LabelText className='label'>{label}</LabelText>
       </Label>
     );
   },
