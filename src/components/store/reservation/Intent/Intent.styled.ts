0b2c6dcebbf222996e50b0e23d4386ed6e96470a
wooyoung6685 <wooyoung6685@gmail.com>
2023-09-07 12:57:57 +0900

    [add] intent 컴포넌트 생성

--- src/components/store/reservation/Intent/Intent.styled.ts
+++ src/components/store/reservation/Intent/Intent.styled.ts
@@ -2,8 +2,17 @@ import styled from 'styled-components';
 
 export const IntentWrapper = styled.div`
   width: 100%;
+  margin-top: 3.2rem;
 `;
 
 export const Form = styled.form`
   width: 100%;
+`;
+
+export const InputRadioGroup = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 1.8rem;
 `;
--- src/components/store/reservation/Intent/Intent.tsx
+++ src/components/store/reservation/Intent/Intent.tsx
@@ -1,11 +1,27 @@
+import { Button } from 'components/form/atoms/Button';
+import { InputLabel } from 'components/form/atoms/InputLabel';
+import { InputRadio } from 'components/form/atoms/InputRadio';
 import { Inputs } from 'components/form/molecules/Inputs';
 import {
-  Form,
+  InputRadioGroup,
   IntentWrapper,
 } from 'components/store/reservation/Intent/Intent.styled';
-import React from 'react';
+import { ButtonWrapper } from 'pages/RootPage/RootPage.styled';
+import { useState } from 'react';
+import { useTheme } from 'styled-components';
 
 export const Intent = () => {
+  const [checkedStatus, setCheckedStatus] = useState<Record<string, boolean>>(
+    {},
+  );
+  const theme = useTheme();
+
+  const handleRadioClick = (name: string) => {
+    setCheckedStatus({
+      ...checkedStatus,
+      [name]: !checkedStatus[name],
+    });
+  };
   return (
     <IntentWrapper>
       <Inputs
@@ -15,6 +31,41 @@ export const Intent = () => {
       >
         사용목적
       </Inputs>
+      <InputRadioGroup>
+        <InputLabel labelRequired>대여장비</InputLabel>
+        <InputRadio
+          id='1'
+          value='모니터'
+          label='모니터'
+          name='모니터'
+          size='small'
+          checked={checkedStatus['모니터']}
+          onClick={() => handleRadioClick('모니터')}
+        />
+        <InputRadio
+          id='2'
+          value='마우스'
+          label='마우스'
+          name='마우스'
+          size='small'
+          checked={checkedStatus['마우스']}
+          onClick={() => handleRadioClick('마우스')}
+        />
+        <InputRadio
+          id='3'
+          value='노트북'
+          label='노트북'
+          name='노트북'
+          size='small'
+          checked={checkedStatus['노트북']}
+          onClick={() => handleRadioClick('노트북')}
+        />
+      </InputRadioGroup>
+      <ButtonWrapper>
+        <Button backgroundColor={theme.palette.primary.orange} color='white'>
+          사용신청
+        </Button>
+      </ButtonWrapper>
     </IntentWrapper>
   );
 };
