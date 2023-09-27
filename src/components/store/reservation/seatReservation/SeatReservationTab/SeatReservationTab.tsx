0d45dbc8809418488ddb3c98cfaceff2a3c904b0
wooyoung6685 <wooyoung6685@gmail.com>
2023-09-27 13:41:55 +0900

    [add] 좌석 예약 탭 생성

---
+++ src/components/store/reservation/seatReservation/SeatReservationTab/SeatReservationTab.tsx
@@ -0,0 +1,69 @@
+import { getSeatReservations } from 'api/reservation/reservation';
+import { Tab } from 'components/common/tab/Tab';
+import { TabPanel } from 'components/common/tab/TabPanel';
+import { Tabs } from 'components/common/tab/Tabs';
+
+import { SeatBooking } from 'components/store/reservation/seatReservation/SeatBooking';
+
+import { SeatUseNow } from 'components/store/reservation/seatReservation/SeatUseNow';
+import { useEffect, useState } from 'react';
+import type { ReservationParams } from 'api/reservation/reservation';
+
+import type { VFC } from 'common/utils/types';
+
+interface ReservationTabProps {
+  value: number;
+  onChange: (value: number) => void;
+  selectedDate: Date;
+}
+export interface ReservationProps {
+  startSchedule: string;
+  endSchedule: string;
+}
+
+export const SeatReservationTab: VFC<ReservationTabProps> = ({
+  value,
+  onChange,
+  selectedDate,
+}) => {
+  const [reservations, setReservations] = useState<ReservationProps[]>([]);
+  const handleValueChange = (newValue: number) => {
+    onChange(newValue);
+  };
+
+  useEffect(() => {
+    const getReservationTime = async () => {
+      try {
+        const kstDate = new Date(selectedDate);
+        kstDate.setHours(kstDate.getHours() + 9);
+
+        let kstIsoString = kstDate.toISOString();
+
+        kstIsoString = kstIsoString.replace(/\.\d+Z$/, '');
+
+        const params: ReservationParams = {
+          'reservation-date-and-time': kstIsoString,
+        };
+        const response = await getSeatReservations(58, params);
+        setReservations(response.result.allReservationsForSeatAndDate);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getReservationTime();
+  }, [selectedDate]);
+  return (
+    <>
+      <Tabs value={value} onChange={handleValueChange}>
+        <Tab label='바로 사용' />
+        <Tab label='예약 하기' />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <SeatUseNow selectedDate={selectedDate} reservations={reservations} />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <SeatBooking selectedDate={selectedDate} reservations={reservations} />
+      </TabPanel>
+    </>
+  );
+};
