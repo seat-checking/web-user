aa58d4785ee589dc902a132fc1f1f0fe7c770259
Sumin Sohn <soyw31@gmail.com>
2023-10-01 16:01:43 +0900

    [feat] Button 컴포넌트 중앙 정렬, 기본 색상 변경

--- src/components/form/atoms/Button/Button.styled.ts
+++ src/components/form/atoms/Button/Button.styled.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   max-width: 67.5rem;
   width: 100%;
+  text-align: center;
 `;
 
 export const ButtonBox = styled.button`
@@ -14,10 +15,12 @@ export const ButtonBox = styled.button`
   font-weight: 500;
   font-size: 1.6rem;
   margin-bottom: 1.6rem;
-  color: ${(props): string => props.theme.palette.grey[400]};
-  background-color: ${(props): string => props.theme.palette.grey[100]};
+  color: ${(props): string => props.theme.palette.white.main};
+  background-color: ${(props): string => props.theme.palette.primary.orange};
 
   &:disabled {
     pointer-events: none;
+    color: ${(props): string => props.theme.palette.grey[400]};
+    background-color: ${(props): string => props.theme.palette.grey[100]};
   }
 `;
