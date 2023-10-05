ce3a53b2d89f430e4c4b1cdbd23097fb2a53d107
wooyoung6685 <wooyoung6685@gmail.com>
2023-10-05 17:32:53 +0900

    [add] 헤더 생성

--- src/pages/JoinSpacePage/JoinSpacePage.tsx
+++ src/pages/JoinSpacePage/JoinSpacePage.tsx
@@ -1,6 +1,20 @@
+import { BackButton } from 'components/common/BackButton';
+import {
+  HeaderWrapper,
+  ReservationPageHeader,
+  ReservationPageTitle,
+} from 'pages/SeatUsePage/SeatUsePage.styled';
+
 /**
  * 스페이스 참여 페이지
  */
 export const JoinSpacePage: React.FC = () => {
-  return <div>스페이스 참여</div>;
+  return (
+    <HeaderWrapper>
+      <ReservationPageHeader>
+        <BackButton />
+        <ReservationPageTitle>스페이스 참여</ReservationPageTitle>
+      </ReservationPageHeader>
+    </HeaderWrapper>
+  );
 };
