ec9caaa24f915aee319bf7f4ff619359253f1c31
wooyoung6685 <wooyoung6685@gmail.com>
2023-10-15 13:25:38 +0900

    [add] 좌석번호&스페이스번호 없을시 접근 불능

--- src/pages/SeatUsePage/SeatUsePage.tsx
+++ src/pages/SeatUsePage/SeatUsePage.tsx
@@ -1,3 +1,4 @@
+import { PATH } from 'common/utils/constants';
 import { BackButton } from 'components/common/BackButton';
 import { SeatUse } from 'components/store/reservation/seatReservation/SeatUse';
 import {
@@ -5,8 +6,18 @@ import {
   ReservationPageHeader,
   ReservationPageTitle,
 } from 'pages/SeatUsePage/SeatUsePage.styled';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useReservationStore } from 'store/reservationStore';
 
 export const SeatUsePage = () => {
+  const storeChairId = useReservationStore((state) => state.storeChairId);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (storeChairId === 1) {
+      navigate(PATH.storeList);
+    }
+  }, [storeChairId, navigate]);
   return (
     <>
       <HeaderWrapper>
--- src/pages/SpaceUsePage/SpaceUsePage.tsx
+++ src/pages/SpaceUsePage/SpaceUsePage.tsx
@@ -1,3 +1,4 @@
+import { PATH } from 'common/utils/constants';
 import { BackButton } from 'components/common/BackButton';
 import { SpaceUse } from 'components/store/reservation/spaceReservation/SpaceUse';
 
@@ -6,8 +7,18 @@ import {
   ReservationPageHeader,
   ReservationPageTitle,
 } from 'pages/SeatUsePage/SeatUsePage.styled';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useReservationStore } from 'store/reservationStore';
 
 export const SpaceUsePage = () => {
+  const storeSpaceId = useReservationStore((state) => state.storeSpaceId);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (storeSpaceId === 1) {
+      navigate(PATH.storeList);
+    }
+  }, [storeSpaceId, navigate]);
   return (
     <>
       <HeaderWrapper>
